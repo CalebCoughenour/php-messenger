9afabc59a9e0066c5e6b071e95e58f6ffd2e5256
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-23 13:38:35 -0700

    add day of week display with php

--- index.php
+++ index.php
@@ -3,11 +3,35 @@
 <head>
   <meta charset="UTF-8" />
   <title>Mock title</title>
+  <link rel="stylesheet" href="styles.css" type="text/css">
 </head>
 <body>
 
 <?php
-  
+  $dayofweek = date("w");
+
+  switch ($dayofweek) {
+    case 0:
+      echo "It is Sunday!";
+      break;
+    case 1:
+      echo "It is Monday!";
+      break;
+    case 2:
+      echo "It is Tuesday!";
+      break;
+    case 3:
+      echo "It is Wednesday!";
+    case 4:
+      echo "It is Thursday!";
+      break;
+    case 5:
+      echo "<p>It is Friday!</p>";
+      break;
+    case 6:
+      echo "It is Saturday!";
+      break;  
+  }
 ?>
 
 </body>
